25162ea04aab84a8e0bb043707906b8076be7262
olehkyshlyan <37051231+olehkyshlyan@users.noreply.github.com>
2019-03-28 01:26:05 +0200

    UP | Articles | Vybory | 2019.03.28
    User JS-code for the page 'Articles | Vybory' of the site vybory.pravda.com.ua

--- vybory.pravda.com.ua.articles.user.js
+++ vybory.pravda.com.ua.articles.user.js
@@ -11,65 +11,7 @@
 // @grant        none
 // ==/UserScript==
 
-var NodesCircuit = new function(){
-  this.circuit = function(pn,cn){
-    var foundNode = null;
-    var howSearch;
-    var tagAttr;
-    var whatSearch;
-    var parentNode = pn;
-    var childNode = cn;
-
-    if(parentNode != undefined){
-      if(parentNode != null){
-        if(childNode != undefined){
-          if(childNode.childNode != undefined){
-            if(childNode.childNode.how != undefined){
-              howSearch = childNode.childNode.how;
-            }
-            if(childNode.childNode.attr != undefined){
-              tagAttr = childNode.childNode.attr;
-            }
-            if(childNode.childNode.what != undefined){
-              whatSearch = childNode.childNode.what;
-            }
-          }
-
-          if(howSearch != undefined){
-            if(whatSearch != undefined){
-              for(property of parentNode.children){
-                if(howSearch == 'tagName'){
-                  if(property.tagName == whatSearch){
-                    foundNode = property;
-                    break;
-                  }
-                }
-                if(howSearch == 'className'){
-                  if(property.className == whatSearch){
-                    foundNode = property;
-                    break;
-                  }
-                }
-                if(howSearch == 'indexOf'){
-                  if(tagAttr != undefined){
-                    if(property[tagAttr].indexOf(whatSearch) != -1){
-                      foundNode = property;
-                      break;
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    return foundNode;
-  }
-}
-
-var UPArticles = new function(){
+var UPArticlesVybory = new function(){
 
   this.body = function(){
     var bodyInclosure = function(){
@@ -124,119 +66,73 @@ var UPArticles = new function(){
 
   this.postComments = function(){
     var postCommentsInclosure = function(){
-      var body = document.body;
-      if(body != undefined){
-        var layout = NodesCircuit.circuit(body,{childNode:{how:'className',what:'layout'}});
-        var mainContent = NodesCircuit.circuit(layout,{childNode:{how:'className',what:'main-content'}});
-        var layoutMain = NodesCircuit.circuit(mainContent,{childNode:{how:'className',what:'layout-main'}});
-        var clearfix = NodesCircuit.circuit(layoutMain,{childNode:{how:'indexOf',attr:'className',what:'clearfix'}});
-        var articleHeader = NodesCircuit.circuit(clearfix,{childNode:{how:'className',what:'article__header'}});
-        var postStatistic = NodesCircuit.circuit(articleHeader,{childNode:{how:'className',what:'post__statistic'}});
-        var postComments = NodesCircuit.circuit(postStatistic,{childNode:{how:'className',what:'post__comments'}});
-        if(postComments != null){
-          postComments.style.display = 'none';
-        }
-      }
+      jQuery(function(){
+        var postComments = jQuery("BODY > DIV[class*='layout'] > DIV.main-content > DIV.layout-main DIV.post__comments");
+        postComments.hide();
+      });
     }
     setTimeout(postCommentsInclosure,2000);
   }
 
+  this.postSocialContainer = function(){
+    var postSocialContainerInclosure = function(){
+      jQuery(function(){
+        var postSocialContainer = jQuery("BODY > DIV[class*='layout'] > DIV.main-content > DIV.layout-main > DIV[class*='cols'] > DIV.col__wide DIV.post__social-container");
+        postSocialContainer.hide();
+      });
+    }
+    setTimeout(postSocialContainerInclosure,2100);
+  }
+
   this.postArticle = function(){
     var postArticleInclosure = function(){
-      var body = document.body;
-      if(body != undefined){
-        var layout = NodesCircuit.circuit(body,{childNode:{how:'className',what:'layout'}});
-        var mainContent = NodesCircuit.circuit(layout,{childNode:{how:'className',what:'main-content'}});
-        var layoutMain = NodesCircuit.circuit(mainContent,{childNode:{how:'className',what:'layout-main'}});
-        var clearfix = NodesCircuit.circuit(layoutMain,{childNode:{how:'indexOf',attr:'className',what:'clearfix'}});
-        var colFluid = NodesCircuit.circuit(clearfix,{childNode:{how:'className',what:'col__fluid'}});
-        var colFluidInner = NodesCircuit.circuit(colFluid,{childNode:{how:'className',what:'col__fluid__inner'}});
-        var postArticle = NodesCircuit.circuit(colFluidInner,{childNode:{how:'indexOf',attr:'className',what:'post_article'}});
-        for(property of postArticle.children){
-          if(property.className == 'post__social-container'){
-            property.style.display = 'none';
-          }
-
-          if(property.className == 'post__source'){
-            property.style.display = 'none';
-          }
-
-          if(property.className == 'post__social_bottom'){
-            property.style.display = 'none';
-          }
-
-          if(property.className == 'unit_bottom-banner'){
-            property.style.display = 'none';
-          }
-
-          if(property.tagName == 'A' && property.name == 'comments'){
-            property.style.display = 'none';
-          }
-
-          if(property.className == 'block_comments'){
-            property.style.display = 'none';
-          }
+      var postArticle = jQuery("BODY > DIV[class*='layout'] > DIV.main-content > DIV.layout-main > DIV[class*='cols'] > DIV.col__wide DIV[class*='post_article']");
+      postArticle.children().each(function(index,element){
+        if(element.className == 'post__source'){
+          element.style.display = 'none';
         }
-      }
+
+        if(element.className == 'post__social_bottom'){
+          element.style.display = 'none';
+        }
+
+        if(element.tagName == 'A' && element.name == 'comments'){
+          element.style.display = 'none';
+        }
+
+        if(element.className == 'block_comments'){
+          element.style.display = 'none';
+        }
+      });
     }
-    setTimeout(postArticleInclosure,2000);
+    setTimeout(postArticleInclosure,2100);
   }
 
   this.postNewsText = function(){
     var postNewsTextInclosure = function(){
-      var body = document.body;
-      if(body != undefined){
-        var layout = NodesCircuit.circuit(body,{childNode:{how:'className',what:'layout'}});
-        var mainContent = NodesCircuit.circuit(layout,{childNode:{how:'className',what:'main-content'}});
-        var layoutMain = NodesCircuit.circuit(mainContent,{childNode:{how:'className',what:'layout-main'}});
-        var clearfix = NodesCircuit.circuit(layoutMain,{childNode:{how:'indexOf',attr:'className',what:'clearfix'}});
-        var colFluid = NodesCircuit.circuit(clearfix,{childNode:{how:'className',what:'col__fluid'}});
-        var colFluidInner = NodesCircuit.circuit(colFluid,{childNode:{how:'className',what:'col__fluid__inner'}});
-        var postArticle = NodesCircuit.circuit(colFluidInner,{childNode:{how:'indexOf',attr:'className',what:'post_article'}});
-        var postNewsText = NodesCircuit.circuit(postArticle,{childNode:{how:'className',what:'post_news__text'}});
-        for(property of postNewsText.children){
-          if(property.tagName == 'P' && property.innerHTML == ''){
-            property.style.display = 'none';
+      var postNewsText = jQuery("BODY > DIV[class*='layout'] > DIV.main-content > DIV.layout-main > DIV[class*='cols'] > DIV.col__wide DIV[class*='post_article'] > DIV.post_news__text");
+      postNewsText.children().each(function(index,element){
+        if(element.tagName == 'P' && element.innerHTML == ''){
+          element.style.display = 'none';
+        }
+
+        if(element.tagName == 'DIV' && element.className.indexOf('image-box') != -1){
+          for(property of element.children){
+            if(property.className == 'image-box__author' && property.innerHTML == ''){
+              property.style.display = 'none';
+            }
           }
         }
-      }
+      });
     }
     setTimeout(postNewsTextInclosure,2000);
   }
 
-  this.colSidebar = function(){
-    var colSidebarInclosure = function(){
-      var body = document.body;
-      if(body != undefined){
-        var layout = NodesCircuit.circuit(body,{childNode:{how:'className',what:'layout'}});
-        var mainContent = NodesCircuit.circuit(layout,{childNode:{how:'className',what:'main-content'}});
-        var layoutMain = NodesCircuit.circuit(mainContent,{childNode:{how:'className',what:'layout-main'}});
-        var clearfix = NodesCircuit.circuit(layoutMain,{childNode:{how:'indexOf',attr:'className',what:'clearfix'}});
-        var colSidebar = NodesCircuit.circuit(clearfix,{childNode:{how:'className',what:'col__sidebar'}});
-
-        for(property of colSidebar.children){
-          if(property.className == 'unit_main-banner_right'){
-            property.style.display = 'none';
-          }
-
-          if(property.className == 'unit_second-banner_right'){
-            property.style.display = 'none';
-          }
-
-          if(property.className == 'unit_third-banner_right'){
-            property.style.display = 'none';
-          }
-        }
-      }
-    }
-    setTimeout(colSidebarInclosure,3000);
-  }
-
 }
 
-UPArticles.body();
-UPArticles.layout();
-UPArticles.postComments();
-UPArticles.postArticle();
-UPArticles.postNewsText();
-UPArticles.colSidebar();
+UPArticlesVybory.body();
+UPArticlesVybory.layout();
+UPArticlesVybory.postComments();
+UPArticlesVybory.postSocialContainer();
+UPArticlesVybory.postArticle();
+UPArticlesVybory.postNewsText();
