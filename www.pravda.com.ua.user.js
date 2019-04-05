11baa63b27dafeb0905b3ed366d3bb50902059e5
olehkyshlyan <37051231+olehkyshlyan@users.noreply.github.com>
2019-04-06 00:52:20 +0300

    UP | Main page | 2019.04.06
    User JS-code for the index page of the site www.pravda.com.ua

--- www.pravda.com.ua.user.js
+++ www.pravda.com.ua.user.js
@@ -22,9 +22,41 @@ var UkrainskaPravda = new function(){
         }
       });
     }
-    setTimeout(unitHeaderBannerInclosure,3000);
+    setTimeout(unitHeaderBannerInclosure,2000);
+  }
+
+  this.unitTopBannerMain = function(){
+    var unitTopBannerMainInclosure = function(){
+      jQuery(function(){
+        var unitTopBannerMain = jQuery("BODY > DIV.layout > DIV.main-content > DIV.unit_top-banner_main");
+        if(unitTopBannerMain.length == 1){
+          unitTopBannerMain.hide();
+        }
+      });
+    }
+    setTimeout(unitTopBannerMainInclosure,2000);
+  }
+
+  this.topUnitMainBannerRight = function(){
+    var topUnitMainBannerRightInclosure = function(){
+      jQuery(function(){
+        var unitSideBanner = jQuery("BODY > DIV.layout > DIV.main-content > DIV.layout-main > DIV[class*='clearfix'] > DIV[class*='col__sidebar'] > DIV.unit_main-banner_right1 > DIV.unit_side-banner");
+        unitSideBanner.children().each(function(index,element){
+          if(element.className == 'advtext'){
+            element.style.cssText += '; display: none !important;';
+          }
+
+          if(element.id.indexOf('adriver') != -1){
+            element.style.cssText += '; display: none !important;';
+          }
+        });
+      });
+    }
+    setTimeout(topUnitMainBannerRightInclosure,3000);
   }
 
 }
 
 UkrainskaPravda.unitHeaderBanner();
+UkrainskaPravda.unitTopBannerMain();
+UkrainskaPravda.topUnitMainBannerRight();
