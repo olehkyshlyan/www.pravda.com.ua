72413c01ec4b9b727bac4ed9fd32ef22d8291abc
olehkyshlyan <37051231+olehkyshlyan@users.noreply.github.com>
2022-03-07 10:14:29 +0200

    UP | News | 2022.03.07
    User JS-code for the page 'News' of the site www.pravda.com.ua

--- www.pravda.com.ua.news.user.js
+++ www.pravda.com.ua.news.user.js
@@ -7,6 +7,7 @@
 // @author       oleh.kyshlyan
 // @match        http://www.pravda.com.ua/news/*
 // @match        https://www.pravda.com.ua/news/*
+// @match        https://*.pravda.com.ua/news/*
 // @require      https://code.jquery.com/jquery-3.4.0.min.js
 // @grant        none
 // ==/UserScript==
@@ -19,11 +20,11 @@ var UPNews20200501 = new function(){
         var body = jQuery("BODY");
         if(body.length == 1){
           body.children().each(function(index,element){
-            if(element.className.indexOf('unit_header_banner') != -1){
-              element.style.display = 'none';
+            if(element.id.indexOf('ar_') == 0){
+              element.style.cssText += '; display: none !important;';
             }
 
-            if(element.className.indexOf('unit_top-banner') != -1){
+            if(element.className.indexOf('banner') != -1){
               element.style.display = 'none';
             }
 
@@ -34,7 +35,24 @@ var UPNews20200501 = new function(){
             if(element.id == 'fb-root'){
               element.style.display = 'none';
             }
+
+            if(element.tagName == 'IFRAME'){
+              element.style.display = 'none';
+            }
+
+            if(element.tagName == 'IMG'){
+              element.style.display = 'none';
+            }
+
+            if(element.id.indexOf('push') != -1){
+              element.style.display = 'none';
+            }
           });
+
+          body.css({"background":"transparent none repeat scroll 0% 0%"});
+          body.css({"padding-top":"0px"});
+          //body.attr({style:""});
+          body.attr({class:""});
         }
       });
     }
@@ -46,6 +64,8 @@ var UPNews20200501 = new function(){
       jQuery(function(){
         var containerSubPostNews = jQuery("BODY > DIV[class*='main_content'] DIV[class*='container_sub_post_news']");
         if(containerSubPostNews.length == 1){
+          containerSubPostNews.css({"max-width":"100%"});
+
           containerSubPostNews.children().each(function(index,element){
             if(element.className.indexOf('banner') != -1){
               element.style.display = 'none';
@@ -62,9 +82,13 @@ var UPNews20200501 = new function(){
       jQuery(function(){
         var containerSubSideNews = jQuery("BODY > DIV[class*='main_content'] > DIV[class*='container_news'] > DIV[class*='container_sub_side_news']");
         if(containerSubSideNews.length == 1){
+          containerSubSideNews.css({"max-width":"none"});
+          containerSubSideNews.css({"margin-left":"0px"});
+          containerSubSideNews.css({"width":"calc(25% + 120px)"});
+
           containerSubSideNews.children().each(function(index,element){
             if(element.className.indexOf('banner') != -1){
-              element.style.display = 'none';
+              element.style.cssText += '; display: none !important;';
             }
           });
         }
@@ -80,7 +104,7 @@ var UPNews20200501 = new function(){
         if(containerSubAdvNews.length == 1){
           containerSubAdvNews.children().each(function(index,element){
             if(element.className.indexOf('banner') != -1){
-              element.style.display = 'none';
+              element.style.cssText += '; display: none !important;';
             }
           });
         }
@@ -103,21 +127,66 @@ var UPNews20200501 = new function(){
             }
 
             if(element.className.indexOf('banner') != -1){
-              element.style.display = 'none';
+              element.style.cssText += '; display: none !important;';
             }
 
             if(element.tagName == 'A' && element.name == 'comments'){
-              element.style.display = 'none';
+              element.style.cssText += '; display: none !important;';
             }
 
             if(element.className.indexOf('post_comments') != -1){
-              element.style.display = 'none';
+              element.style.cssText += '; display: none !important;';
             }
           });
         }
       });
     }
     setTimeout(articleInclosure,3000);
+  }
+
+  this.postText = function(){
+    var postTextInclosure = function(){
+      jQuery(function(){
+        var postText = jQuery("BODY > DIV[class*='main_content'] ARTICLE[class*='post'] DIV[class*='block_post'] > DIV[class*='post_text']");
+        if(postText.length == 1){
+          postText.children().each(function(index,element){
+            if(element.id.indexOf('banner') != -1 || element.className.indexOf('banner') != -1){
+              element.style.cssText += '; display: none !important;';
+            }
+
+            if(element.id.indexOf('admixer') != -1){
+              element.style.cssText += '; display: none !important;';
+            }
+
+            if(element.tagName == 'P' && element.innerHTML == ''){
+              element.style.cssText += '; display: none !important;';
+            }
+
+            if(element.className.indexOf('image-box') != -1){
+              var caption = jQuery(element).children("DIV[class*='caption']");
+              var capInHTML = caption.html();
+              if(capInHTML == ''){
+                caption.hide();
+              }
+
+              var author = jQuery(element).children("DIV[class*='author']");
+              var authInHTML = author.html();
+              if(authInHTML == ''){
+                author.hide();
+              }
+            }
+
+            if(element.tagName == 'BLOCKQUOTE'){
+              var subscription = jQuery(element).find("A:contains('Читайте нас у Telegram')");
+              if(postText.length == 1){
+                element.style.cssText += '; display: none !important;';
+              }
+            }
+          });
+        }
+      });
+    }
+    setTimeout(postTextInclosure,3000);
   }
 
 }
@@ -127,3 +196,4 @@ UPNews20200501.containerSubPostNews();
 UPNews20200501.containerSubSideNews();
 UPNews20200501.containerSubAdvNews();
 UPNews20200501.article();
+UPNews20200501.postText();
